16ad14770474bc45e39100c93f95a25625316433
Dustin Martinez <martinezdustin736@gmail.com>
2020-02-27 21:49:28 -0600

    Updates

---
+++ precache-manifest.06581232df91073c969be44aa4818605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4e44460f6137986b4473f459cbf200",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "9c6cc41c7063e4fc03a4",
+    "url": "/clicky/static/css/main.a05aacf0.chunk.css"
+  },
+  {
+    "revision": "9b19f1c0284bddea7681",
+    "url": "/clicky/static/js/2.219d769e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky/static/js/2.219d769e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6cc41c7063e4fc03a4",
+    "url": "/clicky/static/js/main.6a018245.chunk.js"
+  },
+  {
+    "revision": "0a0fde91fb061a5eb17e",
+    "url": "/clicky/static/js/runtime-main.ebfc6ff2.js"
+  }
+]);
